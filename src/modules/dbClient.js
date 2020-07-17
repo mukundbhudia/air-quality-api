98b67a400158deb0afb84665b85e4e5c015f63f4
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-07-17 11:04:31 +0100

    Fixed issue where Redis port would not be set as env variable

--- src/modules/dbClient.js
+++ src/modules/dbClient.js
@@ -2,10 +2,11 @@ const redis = require("redis")
 const logger = require('./logger').initLogger()
 
 let client
+const PORT = process.env.REDIS_URL || 6379
 
 const connectDB = async () => {
   try {
-    client = redis.createClient()
+    client = redis.createClient(PORT)
   } catch (error) {
     console.error(error)
     logger.error(error)
