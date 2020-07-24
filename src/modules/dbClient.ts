ce01ae4c01ec85075a60a3bcff760b7855e66612
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-07-24 17:59:09 +0100

    Fixed type error for REDIS_URL env var

--- src/modules/dbClient.ts
+++ src/modules/dbClient.ts
@@ -3,11 +3,11 @@ import { initLogger } from './logger'
 initLogger()
 
 let client: any
-const REDIS_PORT: number = parseInt(process.env.REDIS_URL) || 6379
+const REDIS_URL: string|number = parseInt(process.env.REDIS_URL) || 6379
 
 export const connectDB = async (): Promise<any> => {
   try {
-    client = redis.createClient(REDIS_PORT)
+    client = redis.createClient(REDIS_URL)
   } catch (error) {
     console.error(error)
     // logger.error(error)
